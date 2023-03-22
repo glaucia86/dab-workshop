816cd48650323487448b4de7bd4fe5fa2cd5709c
glaucia86 <glaucia_lemos86@hotmail.com>
2023-03-22 18:26:35 -0300

    chore: including 'dbo.employee' sql script

---
+++ demo-01/dbo.employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[employee] (
+    [employee_id] UNIQUEIDENTIFIER NOT NULL,
+    [name] NVARCHAR(100) NOT NULL,
+    [job_role] NVARCHAR(100) NOT NULL,
+    [salary] DECIMAL(12,2) NOT NULL,
+    [employee_registration] INT NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Employee_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updateAt] DATETIME2 NOT NULL CONSTRAINT [Employee_updateAt_df] DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT [Employee_pkey] PRIMARY KEY CLUSTERED ([employee_id]),
+    CONSTRAINT [Employee_employee_registration_key] UNIQUE NONCLUSTERED ([employee_registration])
+);
