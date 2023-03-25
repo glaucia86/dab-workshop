42285f55415a5e27b3182f55ea334d2ead1f6b35
glaucia86 <glaucia_lemos86@hotmail.com>
2023-03-24 21:06:02 -0300

    fix: some important changes in the 'dbo.employees' script

--- demo-01/dbo.employees.sql
+++ demo-01/dbo.employees.sql
@@ -1,11 +1,9 @@
 CREATE TABLE [dbo].[employees] (
-    [employee_id] UNIQUEIDENTIFIER NOT NULL,
+    [employee_id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     [name] NVARCHAR(100) NOT NULL,
     [job_role] NVARCHAR(100) NOT NULL,
     [salary] DECIMAL(12, 2) NOT NULL,
-    [employee_registration] INT NOT NULL,
+    [employee_registration] INT NOT NULL CONSTRAINT [Employee_employee_registration_key] UNIQUE NONCLUSTERED,
     [createdAt] DATETIME2 NOT NULL CONSTRAINT [Employee_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
-    [updateAt] DATETIME2 NOT NULL CONSTRAINT [Employee_updateAt_df] DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT [Employee_pkey] PRIMARY KEY CLUSTERED ([employee_id]),
-    CONSTRAINT [Employee_employee_registration_key] UNIQUE NONCLUSTERED ([employee_registration])
+    [updateAt] DATETIME2 NOT NULL CONSTRAINT [Employee_updateAt_df] DEFAULT CURRENT_TIMESTAMP
 );
